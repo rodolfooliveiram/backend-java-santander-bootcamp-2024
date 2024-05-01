e238fbacf4492393d9b0daa43ac47ce5bae6cbdf
Rodolfo <rodolfoalvesmdo@gmail.com>
2024-05-01 03:02:35 -0300

    Gerando documentação com javadoc

--- 01-sintaxe/src/edu/java/metodos/SmartTv.java
+++ 01-sintaxe/src/edu/java/metodos/SmartTv.java
@@ -1,38 +1,68 @@
 package edu.java.metodos;
 
+/**
+ * <h1>SmartTV App</h1>
+ * App que simula as funcionalidades básicas de uma SmartTV.
+ * 
+ * @author Rodolfo Oliveira
+ * @version 1.0
+ * @since 01/05/2024
+ */
+
 public class SmartTv {
 	
 	public boolean ligada = false;
 	public int canal = 7;
 	public int volume = 15;
 	
+	/**
+	 * Método para ligar a TV.
+	 */
 	public void ligarTv() {
 		ligada = true;
 	}
 	
+	/**
+	 * Método para desligar a TV.
+	 */
 	public void desligarTv() {
 		ligada = false;
 	}
 	
+	/**
+	 * Método para aumentar o volume TV.
+	 */
 	public void aumentarVolume() {
 		volume++;
 	}
 	
+	/**
+	 * Método para diminuir o volume TV.
+	 */
 	public void diminuirVolume() {
 		volume--;
 	}
 	
+	/**
+	 * Método para passar para o canal seguinte ao canal atual.
+	 */
 	public void passarCanalSeguinte() {
 		canal++;
 	}
 	
+	/**
+	 * Método para passar para o canal anterior ao canal atual.
+	 */
 	public void voltarCanalAnterior() {
 		canal--;
 	}
 	
-	public int selecionarCanal(int canalSelecionado) {
+	/**
+	 * Método para mudar o canal atual para um canal específico.
+	 * @param canalSelecionado é número inteiro que indica o canal para o qual deseja mudar.
+	 */
+	public void selecionarCanal(int canalSelecionado) {
 		canal = canalSelecionado;
-		return canal;
 	}
 
 
