db03823fc964eac3f94c977b23e13a2f2cfb0659
Rodolfo <rodolfoalvesmdo@gmail.com>
2024-05-08 00:56:02 -0300

    Desafio de Código n.5

---
+++ 01-desafios-de-codigo/src/edu/java/desafios/controledesaques/ControleSimplesDeSaques.java
@@ -0,0 +1,59 @@
+package edu.java.desafios.controledesaques;
+
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        double limiteDiario = scanner.nextDouble();
+   
+        
+        for(int i = 0; i < limiteDiario; i++) {
+        	double saque = scanner.nextDouble();
+        	
+        	if(saque == 0) {
+        		System.out.println("Transacoes encerradas.");
+        		break;
+        	} else {
+        		if(saque <= limiteDiario) {
+        			limiteDiario -= saque;
+        			System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+        		} else {
+        			System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+        			break;
+        		}
+        	}
+        	
+//        	if(saque > 0) {
+//        		if(saque <= limiteDiario) {
+//        			limiteDiario -= saque;
+//        			System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+//        		} else {
+//        			System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+//        		}
+//        	} else {
+//        		i = (int) limiteDiario;
+//        		System.out.println("Transacoes encerradas.");
+//        	}
+        }
+        
+        scanner.close(); 
+    }
+}
+
+//TODO: Crie um loop 'for' para iterar sobre os saques:
+//TODO: Solicite ao usuário o valor do saque:
+
+
+//TODO: Verifique se o valor do saque é zero, encerrando as transações:
+//Dica: Utilize um 'if/else' para verificar as condições do valorSaque e o limiteDiario;
+
+        
+//TODO: Se o valor do saque não ultrapassar o limite diário, subtraia o valor do saque do limite diário:
+//Fechamos o Scanner para evitar vazamento de recursos:       
+
+//TODO: Informe que o saque foi realizado e mostre o limite restante:
+           
+
--- 01-desafios-de-codigo/src/edu/java/desafios/verificacaochequeespecial/VerificacaoChequeEspecial.java
+++ 01-desafios-de-codigo/src/edu/java/desafios/verificacaochequeespecial/VerificacaoChequeEspecial.java
@@ -12,11 +12,11 @@ public class VerificacaoChequeEspecial {
         double limiteChequeEspecial = 500;
         
         if(saque <= saldo) {
-        	System.out.println("Transação realizada com sucesso.");
+        	System.out.println("Transacao realizada com sucesso.");
         } else if (saque - saldo <= limiteChequeEspecial) {
-        	System.out.println("Transação realizada com sucesso utilizando o cheque especial.");
+        	System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
         } else {
-        	System.out.println("Transação não realizada. Limite do cheque especial excedido.");
+        	System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
         }   
                 
         scanner.close();
